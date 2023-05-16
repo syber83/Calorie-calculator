41a0ba3d44d0ed8ce540432711a62091b2a63f78
syber83 <83616053+syber83@users.noreply.github.com>
2023-05-16 13:47:58 +0300

    More items
    Added extra functionality and more items.

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-const proteinItems = document.querySelectorAll(".protein-item");
+const proteinsBtn = document.querySelector("#proteins-btn");
+const proteinsSection = document.querySelector('#proteins');
 
-proteinItems.forEach((item) => {
+proteinsBtn.addEventListener('click', () => {
+  if (proteinsSection.style.display === 'none') {
+    proteinsSection.style.display = 'block';
+  } else {
+    proteinsSection.style.display = 'none';
+  }
+});
+
+const greensBtn = document.querySelector("#greens-btn");
+const greensSection = document.querySelector('#greens');
+
+greensBtn.addEventListener('click', () => {
+  if (greensSection.style.display === 'none') {
+    greensSection.style.display = 'block';
+  } else {
+    greensSection.style.display = 'none';
+  }
+});
+
+const fruitsBtn = document.querySelector("#fruits-btn");
+const fruitsSection = document.querySelector('#fruits');
+
+fruitsBtn.addEventListener('click', () => {
+  if (fruitsSection.style.display === 'none') {
+    fruitsSection.style.display = 'block';
+  } else {
+    fruitsSection.style.display = 'none';
+  }
+});
+
+const nutsBtn = document.querySelector("#nuts-btn");
+const nutsSection = document.querySelector("#nuts-legumes");
+
+nutsBtn.addEventListener('click', () => {
+  if (nutsSection.style.display === 'none') {
+    nutsSection.style.display = 'block';
+  } else {
+    nutsSection.style.display = 'none';
+  }
+});
+
+// const proteinItems = document.querySelectorAll(".protein-item");
+// const greensItems = document.querySelectorAll(".greens-item");
+// const fruitsItems = document.querySelectorAll(".greens-item");
+// const nutsItems = document.querySelectorAll(".greens-item");
+// (proteinItems, greensItems, fruitsItems, nutsItems) = foodItems;
+// foodItems.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     item.classList.toggle('selected');
+//     calculateCalories();
+//   });
+// });
+
+
+const foodItems = document.querySelectorAll(".protein-item, .greens-item, .fruits-item, .nuts-item");
+foodItems.forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle('selected');
     calculateCalories();
@@ -8,24 +64,22 @@ proteinItems.forEach((item) => {
 });
 
 function calculateCalories() {
-  const selectedProteins = document.querySelectorAll('.protein-item.selected');
+  const selectedFoods = document.querySelectorAll('.selected');
   let totalCalories = 0;
-  let proteinNames = '';
+  let foodNames = '';
   
   // Loop through all the selected protein items and sum up their calories
-  selectedProteins.forEach((protein) => {
-    const calories = parseInt(protein.dataset.calories);
+  selectedFoods.forEach((food) => {
+    const calories = parseInt(food.dataset.calories);
     totalCalories += calories;
-    proteinNames += protein.dataset.name + ' + ';
+    foodNames += food.dataset.name + ' + ';
   });
   
   // Remove the last ' + ' from the proteinNames string
   // proteinNames = proteinNames.substring(0, proteinNames.length - 3);
   
   // Display the message on the calories display div
-  const message = `Your food is ${proteinNames} and your total calories are: ${totalCalories}.`;
+  const message = `Your food is ${foodNames} and your total calories are: ${totalCalories}.`;
   const caloriesDisplay = document.getElementById('calories-display');
   caloriesDisplay.textContent = message;
 }
-
-
